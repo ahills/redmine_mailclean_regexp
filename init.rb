4a194e3e0f7b04a7bffa0703d31546477389160d
Andrew Hills <ahills@ednos.net>
2014-04-01 18:06:27 -0400

    Add basic plugin metadata

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_mail_handler_clean_body_regexp do
   name 'Redmine Mail Handler Clean Body Regexp plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Andrew Hills'
+  description 'A Redmine plugin to allow regular expression body trimming on incoming emails'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://git.ednos.net/~ahills/redmine_mail_handler_clean_body_regexp.git'
+  author_url 'https://ednos.net/'
 end
