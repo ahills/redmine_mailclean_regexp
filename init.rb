7a6ec7d54aa3c35148a762a336053c1f04595ec7
Andrew Hills <ahills@ednos.net>
2014-04-01 23:00:33 -0400

    Set proper version

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_mail_handler_clean_body_regexp do
   name 'Redmine Mail Handler Clean Body Regexp plugin'
   author 'Andrew Hills'
   description 'A Redmine plugin to allow regular expression body trimming on incoming emails'
-  version '0.0.1'
+  version '0.1'
   url 'https://git.ednos.net/~ahills/redmine-mailclean-regexp.git'
   author_url 'https://ednos.net/'
 end
