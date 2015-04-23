b0e04f56cdfebacda0b1d9bd77d60d3526323255
Andrew Hills <ahills@ednos.net>
2015-04-23 18:03:13 -0400

    Update for GitHub

--- init.rb
+++ init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :redmine_mail_handler_clean_body_regexp do
   author 'Andrew Hills'
   description 'A Redmine plugin to allow regular expression body trimming on incoming emails'
   version '0.1'
-  url 'https://git.ednos.net/~ahills/redmine_mailclean_regexp.git'
-  author_url 'https://ednos.net/'
+  url 'https://github.com/ahills/redmine_mailclean_regexp'
+  author_url 'https://github.com/ahills'
 end
